e8c960fde390d849a8d3046e5682b91683df45b5
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2022-08-22 21:20:01 +0530

    sdfgh
    dfghjk

---
+++ lab1 python/fuuncctionn.py
@@ -0,0 +1,17 @@
+# def hello():
+#     print('hellllllllo')
+# hello()
+
+# a=int(input("enter the value"))
+# for i in range (a,0,-1):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print("\n")
+
+# a=int(input("enter the value: "))
+# for i in range (1,a+1):
+#     for k in range(1,a-i):
+#         for j in range(1,i+1):
+#             print("*",end=" ")
+#         print("\n")
+
