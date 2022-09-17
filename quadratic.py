f9301a8c47cc6325068fa98a4ebe6bbd7b0ae908
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2022-09-17 20:45:29 +0530

    resolved issue

--- quadratic.py
+++ quadratic.py
@@ -1,8 +1,10 @@
-#Python program to find roots of quadratic equation
+
+#Python program to find roots of quadratic equatioh
+
 import cmath
 a=float(input("co-efficient of x**2__:"))
 b=float(input("co-efficient of x__:"))
 c=float(input("constant term of equation:"))
 X=(-b+(cmath.sqrt(b**2-4*a*c)))/2*a
 Y=(-b-(cmath.sqrt(b**2-4*a*c)))/2*a
-print("roots of the equation is= ",X Y,)
+print("roots of the equation is= ",X ,Y)
