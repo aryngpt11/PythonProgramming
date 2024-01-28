a2868e93335087ebebe0f53da7a2a440e314c417
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-01-28 10:28:50 +0530

    Question

---
+++ PYTHON_WLH/Question2.py
@@ -0,0 +1,5 @@
+digit=int(input("Enter the number: "))
+if(digit>=1000 and digit<=9999):
+    print("The given number is four digit number" ,digit)
+else:
+    print("The given number is not a Foue didgit number" ,digit)
