75c8920d2c72498c04e228bf889796ead096f652
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-02-03 20:49:23 +0530

    oops

---
+++ OOPS/First.py
@@ -0,0 +1,9 @@
+class Student:
+    def set_name(self,name):
+        self.name=name
+    def get_name(self):
+        return self.name
+student1=Student()
+student1.set_name("Aryan")
+#print(student1.name)
+print(student1.get_name("Ayush"))
--- Stringss/palindroem.py
+++ Stringss/palindroem.py
@@ -1,10 +1,19 @@
-""" def palindrome(s):
-     """
-
-a=input("Enter the string: ")
+#using normal if else 
+""" a=input("Enter the string: ")
 c=a.lower()
 print(c)
 if c==c[::-1]:
     print("yes it is palindrome")
 else:
-    print("not a palindrome")
+    print("not a palindrome") """
+
+#using recusion
+def check_paindrome(str):
+    clean_str=(str.replace(" ","")).lower()
+    reverse_str=clean_str[::-1]
+    return clean_str==reverse_str
+str=input("enter a string")
+if check_paindrome(str)==True:
+    print("PAlindrome")
+else:
+    print("not palindrome")
