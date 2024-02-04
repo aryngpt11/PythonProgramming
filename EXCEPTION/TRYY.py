003aa41285aa71b96c49c635e8d187da538882fc
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-02-04 23:50:43 +0530

    exception

--- EXCEPTION/TRYY.py
+++ EXCEPTION/TRYY.py
@@ -1,1 +1,9 @@
-a=int
+a=int(input("Enter value of a: "))
+b=int(input("Enter value of b: "))
+try:
+    c=a/b
+    print("result",c)
+except:
+    print("cannot divide by zer0")
+else:
+    print("Aryan")
