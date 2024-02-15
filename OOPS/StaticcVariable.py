17d771079d3df9f7278c6f31d2851efab9fe251c
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-02-15 20:20:22 +0530

    static

--- OOPS/StaticcVariable.py
+++ OOPS/StaticcVariable.py
@@ -1,7 +1,16 @@
 class Mobile:
     fp='Yes'    #class Variable
-    @classmethod
+    def __init__(self):
+        self.model='Iphone'    #Instance variable
+    def sm(self):
+        print("Model:",self.model)  #instance method
+
+    @classmethod #class Method
     def is_fp(cls):
-        print(cls.fp)
+        print("finger Print:",cls.fp) #Accessing class varible
 rl=Mobile()
-print(Mobile.fp)
+rl.sm()
+Mobile.is_fp()
+print()
+Mobile.fp='No'
+Mobile.is_fp()
